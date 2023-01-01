7dab11d4c90c70c4d473a6686ee9ffb99cdbb6ca
Troy Chryssos <troychryssos@gmail.com>
2023-01-01 11:35:22 -0500

    email us button

--- src/components/NavBar/MdMenu.tsx
+++ src/components/NavBar/MdMenu.tsx
@@ -3,10 +3,23 @@ import { Flex } from '@mantine/core';
 
 import { ABOUT_ROUTE, HOME_ROUTE } from '~/constants/routing';
 import { getMediaQueryMinWidth } from '~/constants/theme';
+import { pxToRem } from '~/logic/util/styles';
 
 import { Link } from '../Link';
 
-const ContactLink = styled.a``;
+const ContactLink = styled.a`
+  font-size: ${pxToRem(20)};
+  border: 2px solid ${({ theme }) => theme.colors.gray[9]};
+  border-radius: 500px;
+  padding: ${pxToRem(8)} ${pxToRem(16)};
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.gray[9]};
+  :hover {
+    color: ${({ theme }) => theme.colors.blue[6]};
+    border-color: ${({ theme }) => theme.colors.blue[6]};
+    background-color: ${({ theme }) => theme.colors.blue[0]};
+  }
+`;
 
 const MdMenuWrapper = styled(Flex)`
   display: none;
@@ -20,7 +33,9 @@ export function MdMenu() {
     <MdMenuWrapper align="center" gap={40}>
       <Link href={HOME_ROUTE}>Home</Link>
       <Link href={ABOUT_ROUTE}>About</Link>
-      <ContactLink>Contact Us</ContactLink>
+      <ContactLink href="mailto:inquiries@jbrad.co" target="_blank">
+        Email Us
+      </ContactLink>
     </MdMenuWrapper>
   );
 }
