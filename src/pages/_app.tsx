653f190429a1f714c68ccf9022e4d57e95fecef8
Troy Chryssos <troychryssos@gmail.com>
2023-01-02 20:57:43 -0500

    axing color theme cuz we dont need it

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,52 +1,38 @@
-import { ColorSchemeProvider, MantineProvider } from '@mantine/core';
+import { MantineProvider } from '@mantine/core';
 import type { AppProps } from 'next/app';
-import { useState } from 'react';
 
 import { BREAKPOINT_VALUES, CUSTOM_THEME_COLORS } from '~/constants/theme';
-import { ColorScheme } from '~/typings/theme';
 
 function Page({ Component, pageProps }: AppProps) {
-  const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
-
-  const toggleColorScheme = () => {
-    setColorScheme((prev) => (prev === 'dark' ? 'light' : 'dark'));
-  };
-
   return (
-    <ColorSchemeProvider
-      colorScheme={colorScheme}
-      toggleColorScheme={toggleColorScheme}
-    >
-      <MantineProvider
-        theme={{
-          colorScheme,
-          colors: CUSTOM_THEME_COLORS,
-          fontFamily: 'Poppins, sans-serif',
-          breakpoints: {
-            xs: BREAKPOINT_VALUES.xs,
-            sm: BREAKPOINT_VALUES.sm,
-            md: BREAKPOINT_VALUES.md,
-            lg: BREAKPOINT_VALUES.lg,
-            xl: BREAKPOINT_VALUES.xl,
-          },
-          headings: { fontFamily: 'Poppins, sans-serif' },
-          components: {
-            Text: {
-              defaultProps: {
-                component: 'p',
-                p: 0,
-                m: 0,
-              },
+    <MantineProvider
+      theme={{
+        colors: CUSTOM_THEME_COLORS,
+        fontFamily: 'Poppins, sans-serif',
+        breakpoints: {
+          xs: BREAKPOINT_VALUES.xs,
+          sm: BREAKPOINT_VALUES.sm,
+          md: BREAKPOINT_VALUES.md,
+          lg: BREAKPOINT_VALUES.lg,
+          xl: BREAKPOINT_VALUES.xl,
+        },
+        headings: { fontFamily: 'Poppins, sans-serif' },
+        components: {
+          Text: {
+            defaultProps: {
+              component: 'p',
+              p: 0,
+              m: 0,
             },
           },
-        }}
-        withGlobalStyles
-        withNormalizeCSS
-      >
-        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-        <Component {...pageProps} />
-      </MantineProvider>
-    </ColorSchemeProvider>
+        },
+      }}
+      withGlobalStyles
+      withNormalizeCSS
+    >
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
+    </MantineProvider>
   );
 }
 
